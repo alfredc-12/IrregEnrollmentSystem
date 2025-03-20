154f3b5bdf98a58f8213420ec99602d0b13d9e0f
alfredc-12 <davidgludo@gmail.com>
2025-03-20 20:32:39 +0800

    Testing

--- EnrollmentSystem/src/main/java/Application/HelloApplication.java
+++ EnrollmentSystem/src/main/java/Application/HelloApplication.java
@@ -18,7 +18,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+// Hello Everyone!
     public static void main(String[] args) {
         launch();
     }
